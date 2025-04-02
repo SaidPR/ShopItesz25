5397dd8e7128546dfa7ead7f7f9ea167bd6c7a0b
Said Piñones <ramoszaid5@gmail.com>
2025-04-02 09:45:27 -0600

    Codes Xd

--- PedidosREST/main.py
+++ PedidosREST/main.py
@@ -1,18 +1,33 @@
+# Importar la librería de uvicorn para usar un servidor local
 import uvicorn
+# Importar la clase FastAPI del framework
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from router import pedidosROUTER, productosROUTER, usuariosROUTER
+from dao.database import Conexion
 
-# Crear una instancia de la clase FastApi
-app = FastAPI()
+# Manejo del ciclo de vida de la app
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Conexión con MongoDB")
+    conexion = Conexion()
+    app.state.db = conexion.getDB()  # Guardamos la conexión en app.state
+    yield  # Pausa aquí hasta que la app se cierre
+    print("Cerrando la conexión con MongoDB")
+    conexion.cerrar()
+
+# Crear una instancia de FastAPI con el manejador de ciclo de vida
+app = FastAPI(lifespan=lifespan)
+
+# Incluir routers
 app.include_router(pedidosROUTER.router)
 app.include_router(productosROUTER.router)
 app.include_router(usuariosROUTER.router)
 
 @app.get("/")
-async  def home():
-     salida = {"mensaje": "Bienvenido a PEDIDOSREST"}
-     return salida
+async def home():
+    return {"Mensaje": "Bienvenida a PEDIDOSREST"}
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    uvicorn.run("main:app", host='127.0.0.1', reload = True)
+# Ejecutar el servidor
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", reload=True)
